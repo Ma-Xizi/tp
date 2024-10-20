aa89084a9bdf3edc6ec1c585860dd4896edb25c0
Ma-Xizi <124993978+Ma-Xizi@users.noreply.github.com>
2024-10-20 11:51:29 +0800

    Brainstorm on different fields for student

--- src/main/java/tutorease/address/model/person/Student.java
+++ src/main/java/tutorease/address/model/person/Student.java
@@ -3,6 +3,7 @@ package tutorease.address.model.person;
 import static java.util.Objects.requireNonNull;
 import static tutorease.address.model.person.Role.STUDENT;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import tutorease.address.model.tag.Tag;
@@ -12,6 +13,11 @@ import tutorease.address.model.tag.Tag;
  */
 public class Student extends Person {
     private final PersonList<Person> guardians;
+    private String zoomLink;
+    private final Set<String> subjects;
+    private final String gradeLevel;
+    private final double fees;
+    private final String examDetails;
     /**
      * Every field must be present and not null.
      *
@@ -26,6 +32,12 @@ public class Student extends Person {
     public Student(Name name, Phone phone, Email email, Address address, Role role, Set<Tag> tags) {
         super(name, phone, email, address, role, tags);
         this.guardians = new PersonList<Person>();
+        this.zoomLink = "";
+        this.subjects = new HashSet<>();
+        this.gradeLevel = "";
+        this.fees = 0;
+        this.examDetails = "";
+
     }
 
     public Role getRole() {
@@ -48,5 +60,29 @@ public class Student extends Person {
         if (!this.guardians.containPerson(guardian)) {
             this.guardians.addPerson(guardian);
         }
+    }
+
+    public String getZoomLink() {
+        return zoomLink;
+    }
+
+    public void setZoomLink(String zoomLink) {
+        this.zoomLink = zoomLink;
+    }
+
+    public Set<String> getSubjects() {
+        return subjects;
+    }
+
+    public String getGradeLevel() {
+        return gradeLevel;
+    }
+
+    public double getFees() {
+        return fees;
+    }
+
+    public String getExamDetails() {
+        return examDetails;
     }
 }
